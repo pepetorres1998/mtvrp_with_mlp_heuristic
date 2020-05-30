3f0c0880d9810ad85025ba85bb26d3677f3cc6a0
Jose Antonio Torres Garibay <jtorres@syscap.com.mx>
2020-05-30 16:57:50 -0500

    Add principal public function on vrp module

--- lib/vrp/set_file.rb
+++ lib/vrp/set_file.rb
@@ -1,6 +1,20 @@
 require 'pry'
+require_relative '../node'
 
 module VRP
   class SetFile
+    attr_accessor :file
+
+    def initialize(**attrs)
+      @file = attrs[:file]
+
+      start_heuristic if attrs_present?
+    end
+
+    private
+
+    def attrs_present?
+      file.present?
+    end
   end
 end
