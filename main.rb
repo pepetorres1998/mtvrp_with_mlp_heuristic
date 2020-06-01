452fb927442b6202e7e85c497785e178c07063b2
Jose Antonio Torres Garibay <jtorres@syscap.com.mx>
2020-05-31 23:22:40 -0500

    Add different test instance

--- main.rb
+++ main.rb
@@ -2,6 +2,8 @@ require 'pry'
 require_relative 'lib/vrp/file_setter'
 
 lines = File.readlines('./test_instances/MT-DMP10s0-01.txt').map(&:strip).map { |line| line.gsub /\t/, ',' }
+lines2 = File.readlines('./test_instances/VRPNC1m.txt').map(&:strip).map { |line| line.gsub /\t/, ',' }
 VRP::FileSetter.new(lines: lines, type: 'matrix')
+VRP::FileSetter.new(lines: lines2, type: 'coordinates')
 
 puts 'hi'
