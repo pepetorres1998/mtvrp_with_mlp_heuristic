96d965ddcadeea2e203f278b985c0179ea7a89f0
Jose Antonio Torres Garibay <jtorres@syscap.com.mx>
2020-06-02 00:14:40 -0500

    Add heuristic class

--- lib/vrp/heuristic.rb
+++ lib/vrp/heuristic.rb
@@ -1,6 +1,17 @@
 require 'pry'
+require_relative 'file_setter'
 
 module VRP
   class Heuristic
+    attr_accessor :clients, :nodes, :vehicle, :matrix
+
+    def initialize(**attrs)
+      file_setter = VRP::FileSetter.new(**attrs)
+
+      @clients = file_setter.clients
+      @nodes = file_setter.nodes
+      @vehicle = file_setter.vehicle
+      @matrix = file_setter.matrix
+    end
   end
 end
--- main.rb
+++ main.rb
@@ -1,10 +1,11 @@
 require 'pry'
 require_relative 'lib/vrp/file_setter'
+require_relative 'lib/vrp/heuristic'
 
 file = File.expand_path('MT-DMP10s0-01.txt', 'test_instances')
 file2 = File.expand_path('VRPNC1m.txt', 'test_instances')
 
-VRP::FileSetter.new(file: file, type: 'matrix')
-VRP::FileSetter.new(file: file2, type: 'coordinates')
+VRP::Heuristic.new(file: file, type: 'matrix')
+VRP::Heuristic.new(file: file2, type: 'coordinates')
 
 puts 'hi'
