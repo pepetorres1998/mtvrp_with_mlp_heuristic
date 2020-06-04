cfa047286eab371ff3aae82382a7cadba57a88a9
Jose Antonio Torres Garibay <jtorres@syscap.com.mx>
2020-06-04 01:46:37 -0500

    Add Path class

---
+++ lib/path.rb
@@ -0,0 +1,35 @@
+require 'pry'
+require_relative 'Route'
+
+class Path
+  attr_writer :routes
+
+  def initialize(**attrs)
+    @routes = attrs[:routes]
+  end
+
+  def routes
+    @routes.sort_by(&:latency)
+  end
+
+  def add_route(route)
+    self.routes << route if route.is_a? Route
+  end
+
+  def nodes_latencies
+    nodes_latencies = []
+    routes.map.with_index do |route, index|
+      nodes_latencies << if index.zero?
+                           route.nodes_latencies
+                         else
+                           route.nodes_latencies(nodes_latencies[index - 1].last)
+                         end
+    end
+
+    nodes_latencies
+  end
+
+  def latency
+    nodes_latencies.flatten.sum
+  end
+end
